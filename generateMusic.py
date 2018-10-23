6152da1cdaf6cde4a199b5a1aedfd23463a41a1b
Thomas Kluyver <thomas@kluyver.me.uk>
2018-10-23 21:19:20 +0100

    Some cleanup

--- generateMusic.py
+++ generateMusic.py
@@ -3,18 +3,15 @@ import sys
 FREQUENCY = 'etaoinsrhdlucmfywgpbv'
 ORDER = 'vpwfclhsiaetonrdumygb'
 
-
-
 with open(sys.argv[1], 'r') as fp:
     text = fp.read()
 
 wav_filename = sys.argv[1].split('.')[0] + '.wav'
 
 NOTES = (
-    #'g2', 'a3', 'b3',
     'c3', 'd3', 'e3', 'f3', 'g3', 'a4', 'b4',
     'c4', 'd4', 'e4', 'f4', 'g4', 'a5', 'b5',
-    'c5', 'd5', 'e5', 'f5', 'g6', 'a6', 'bb6',
+    'c5', 'd5', 'e5', 'f5', 'g6', 'a6', 'b6',
 )
 
 def generate_music(text):
@@ -31,7 +28,6 @@ for word_notes in generate_music(text):
         score.append((note, n))
     score.append(('r', n))
 
-import sys
 sys.path.append('pysynth')
 from pysynth_b import make_wav
 make_wav(score[:500], bpm = 264, leg_stac = 0.9, boost=1.1, fn=wav_filename)
